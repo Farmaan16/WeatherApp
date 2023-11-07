adb5544eafb0997f0b3ee5b28eefe7dfd3ad23c3
Farmaan16 <farmanmuzammil17@gmail.com>
2023-11-07 17:03:29 +0530

    changes

---
+++ src/app/environment.ts
@@ -0,0 +1,23 @@
+export const environment = {
+    production: false,
+    weatherApiBaseUrl: 'https://openweather43.p.rapidapi.com/weather',
+
+    XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+    XRapidAPIHostHeaderValue: 'openweather43.p.rapidapi.com',
+
+
+    XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+    XRapidAPIKeyHeaderValue: 'fa2bce2e7dmsha46e947025c15b1p1b402ejsn0955bb7757ed'
+
+
+    // weatherApiBaseUrl: 'http://api.weatherstack.com/',
+
+    // XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+    // XRapidAPIHostHeaderValue: 'open-weather28.p.rapidapi.com',
+
+
+    // XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+    // XRapidAPIKeyHeaderValue: 'fa2bce2e7dmsha46e947025c15b1p1b402ejsn0955bb7757ed'
+
+
+}
